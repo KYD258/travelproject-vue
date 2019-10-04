0424bcb7658107757d3f87e95daca62db825943e
KYD258 <1121991039@qq.com>
2019-10-04 10:55:31 +0800

    用户端登录注册前段页面

---
+++ src/components/userLogin.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <el-form :model="user" status-icon :rules="rules" ref="user" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="用户名" prop="userName">
+        <el-input type="userName" v-model="user.userName" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" v-model="user.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('user')">提交</el-button>
+        <el-button @click="resetForm('user')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default{
+      data(){
+        var validateUserName = (rule, value, callback) => {
+          if (value === '') {
+            callback(new Error('请输入用户名'));
+          } else {
+            callback();
+          }
+        };
+        var validatePassword = (rule, value, callback) => {
+          if (value === '') {
+            callback(new Error('请输入密码'));
+          } else {
+            callback();
+          }
+        };
+          return{
+              msg:'用户登录页面',
+              user:{
+                  userName:'',
+                  password:""
+              },
+            rules:{
+              userName: [{ validator: validateUserName, trigger: 'blur' }],
+              password: [{ validator: validatePassword, trigger: 'blur' }]
+            }
+          }
+      },
+    methods:{
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+            axios.post("/api/userLogin",this.user).then(res=>{
+                alert("登录成功")
+            });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
+  }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,18 @@ import Router from 'vue-router'
 import SysUserRegist from '@/components/SysUserRegist'
 import SysUserLogin from '@/components/SysUserLogin'
 import show from '@/components/show'
+import userIndex from '@/components/userIndex'
+import userLogin from '@/components/userLogin'
+import userResgister from '@/components/userResgister'
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/SysUserRegist',
-      name: 'SysUserRegist',
-      component: SysUserRegist
-    },
-    {
-      path: '/',
-      name: 'SysUserLogin',
-      component: SysUserLogin
-    },
-    {
-      path: '/show',
-      name: 'show',
-      component: show
-    }
+    {path: '/SysUserRegist',name: 'SysUserRegist',component: SysUserRegist },
+    {path: '/SysUserLogin',name: 'SysUserLogin',component: SysUserLogin},
+    {path: '/show',name: 'show',component: show},
+    {path:'/',name:'userIndex',component:userIndex},
+    {path:'/userLogin',name:'userLogin',component:userLogin},
+    {path:'/userResgister',name:'userResgister',component:userResgister}
   ]
 })
